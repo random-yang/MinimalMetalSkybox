2dca5f67b5e1a6d2ded50b77cfbfa65afce27d6f
RandomYang <decoderyang@gmail.com>
2025-01-22 11:37:16 +0800

    Initial Commit

---
+++ Metal_skybox/Metal_skyboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_skyboxApp.swift
+//  Metal_skybox
+//
+//  Created by randomyang on 2025/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Metal_skyboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Metal_skyboxTests/Metal_skyboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_skyboxTests.swift
+//  Metal_skyboxTests
+//
+//  Created by randomyang on 2025/1/22.
+//
+
+import Testing
+@testable import Metal_skybox
+
+struct Metal_skyboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
